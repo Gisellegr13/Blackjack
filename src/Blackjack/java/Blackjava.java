73fb93c378f93daeadf24b047aa951c7dd98a1f2
Giselle <58486290+Gisellegr92@users.noreply.github.com>
2022-07-13 16:29:31 -0400

    Initial commit

---
+++ src/Blackjack/java/Blackjava.java
@@ -0,0 +1,15 @@
+package Blackjack.java;
+
+import java.util.Scanner;
+
+class Blackjack {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        int gameNo;
+
+        System.out.println("Hellooo");
+
+
+    }
+}
